5b6c7fd07fd9c9c6364ecf392c5578371e878ca8
Splakadern <77061416+Noob-Junk@users.noreply.github.com>
2021-02-19 19:06:28 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+  $hostname = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "chatapp";
+
+  $conn = mysqli_connect($hostname, $username, $password, $dbname);
+  if(!$conn){
+    echo "Database connection error".mysqli_connect_error();
+  }
+?>
